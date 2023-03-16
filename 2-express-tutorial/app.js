9bc8fbe1199fd38be45bc1ba311102ea5273ab2a
ccsc <chandrachudsingh81@gmail.com>
2023-03-16 23:50:30 +0530

    http & express basics and app & static files

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,1 +1,30 @@
-console.log("express");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
+
+// app.get("/about", (req, res) => {
+//   res.status(200).send("About Page");
+// });
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
